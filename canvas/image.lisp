e08d15af676572b31a99d717008bb99e29ce8468
Pavel Korolev <dev@borodust.org>
2017-03-22 02:18:01 +0300

    Add image flipping for image-paint

--- canvas/image.lisp
+++ canvas/image.lisp
@@ -12,18 +12,24 @@
   (nvg:destroy-image context id))
 
 
-(defmethod initialize-instance :after ((this nvg-image) &key image canvas)
+(defmethod initialize-instance :after ((this nvg-image) &key image canvas flip-vertically-p)
   (assert (eq :rgba (pixel-format-of image)) (image)
           "Only :rgba images supported")
   (with-slots (context id data) this
     (let ((ctx (handle-value-of canvas))
-          (array (simple-array-of (foreign-array-of image))))
+          (array (simple-array-of (foreign-array-of image)))
+          (opts (nconc (list :generate-mipmaps :repeatx :repeaty)
+                       (when flip-vertically-p
+                         (list :flipy)))))
       (setf data (make-foreign-array (length array) :initial-contents array))
       (setf context ctx
-            id (nvg:make-rgba-image ctx (width-of image) (height-of image)
-                                    (foreign-pointer-of data)
-                                    :generate-mipmaps :repeatx :repeaty)))))
+            id (apply #'nvg:make-rgba-image ctx (width-of image) (height-of image)
+                      (foreign-pointer-of data)
+                      opts)))))
 
 
-(defun image->nvg (canvas image)
-  (make-instance 'nvg-image :image image :canvas canvas))
+(defun image->nvg (canvas image &key flip-vertically-p)
+  (make-instance 'nvg-image
+                 :image image
+                 :canvas canvas
+                 :flip-vertically-p flip-vertically-p))
